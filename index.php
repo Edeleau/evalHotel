f5f4306c478e84b052b2da2de24738d0f81b1d3a
Pierre Duda <pierreduda@hotmail.fr>
2021-01-19 16:10:13 +0100

    save4

--- index.php
+++ index.php
@@ -4,32 +4,42 @@ include("./nav.php");
 ?>
 <main>
     <div id="landingPage1" class="container-fluid">
-            <img id="logoLandingPage" src="./assets/img/logo_projet_front.png" alt="logo Hotelcop">
-            <a class="reserveButton" href="#">Réservez</a>
+        <img id="logoLandingPage" src="./assets/img/logo_projet_front.png" alt="logo Hotelcop">
+        <a class="reserveButton" href="./reserve.php">Réservez</a>
     </div>
-    <div id="landingPage2" class="container-fluid">
-            <div class="sectionTitle">
-            <h2>Tour d'horizon</h2>
-            <span class="smallHr"></span>
-            <h3>HOTELCOP</h3>
-            </div>
-            <div id="whoWeAreContainer" class="row">
-            <div id="imgTest1" class="col-md-6 col-sm-12"></div>
-                <div id="whoWeAreText" class="col-md-5 col-sm-12">
-                <h4>Le Prestige Hotelcop</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro accusantium enim mollitia quo aliquid eaque minus magnam repellendus suscipit. Non est beatae sed praesentium omnis repellendus mollitia ullam impedit incidunt.
-                    Laudantium, unde? Temporibus iusto facilis id repellat iste autem nobis fuga quibusdam voluptatem molestias adipisci officia, consequuntur doloribus suscipit fugit numquam molestiae? Laboriosam voluptatem numquam aliquid hic expedita et non.
-                    Voluptatem eaque saepe neque tempore ullam placeat earum nisi atque. Minus magnam repellendus nobis provident voluptates incidunt, culpa in laborum temporibus voluptatibus quidem sed expedita et porro ex accusamus aspernatur.
-                    Error cumque, debitis ut fuga, voluptas tempora consectetur reiciendis maxime id necessitatibus ullam a provident. Laboriosam tempore, alias cumque earum incidunt facilis illo dolorem dolores nesciunt nulla quasi aliquam est?
-                    </p>
-                </div>
-            </div>
-    </div>
-
-
-
-
 </main>
+<section id="landingPage2" class="container-fluid">
+    <div class="sectionTitle">
+        <h2>Tour d'horizon</h2>
+        <div class="smallHr col-12"></div>
+        <h3>HOTELCOP</h3>
+    </div>
+    <div id="whoWeAreContainer" class="row">
+        <div id="imgTest1" class="col-md-6 col-sm-12"></div>
+        <div id="whoWeAreText" class="col-md-5 col-sm-12">
+            <h4>Le Prestige Hotelcop</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro accusantium enim mollitia quo aliquid eaque minus magnam repellendus suscipit. Non est beatae sed praesentium omnis repellendus mollitia ullam impedit incidunt.
+                Laudantium, unde? Temporibus iusto facilis id repellat iste autem nobis fuga quibusdam voluptatem molestias adipisci officia, consequuntur doloribus suscipit fugit numquam molestiae? Laboriosam voluptatem numquam aliquid hic expedita et non.
+                Voluptatem eaque saepe neque tempore ullam placeat earum nisi atque. Minus magnam repellendus nobis provident voluptates incidunt, culpa in laborum temporibus voluptatibus quidem sed expedita et porro ex accusamus aspernatur.
+                Error cumque, debitis ut fuga, voluptas tempora consectetur reiciendis maxime id necessitatibus ullam a provident. Laboriosam tempore, alias cumque earum incidunt facilis illo dolorem dolores nesciunt nulla quasi aliquam est?
+            </p>
+        </div>
+    </div>
+</section>
+<section id="landingPage3" class="container-fluid">
+    <div class="sectionTitle">
+        <h2>Le Séjour</h2>
+        <div class="smallHr col-12"></div>
+        <h3>HOTELCOP</h3>
+    </div>
+    <div id="journey" class="row">
+        <div id="imgRoom2" class="col-md-3 col-sm-12"></div>
+        <div id="imgRoom" class="col-md-6 col-sm-12"></div>
+        <div id="imgRoom3" class="col-md-3 col-sm-12"></div>
+
+    </div>
+    </div>
+</section>
 <?php
 include("./footer.php");
 ?>
