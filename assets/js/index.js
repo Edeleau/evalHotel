d7878f0c79e870a00dd7eae3ff332ff0822b1516
Pierre Duda <pierreduda@hotmail.fr>
2021-01-20 14:20:19 +0100

    fixsave7

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,43 +1,10 @@
-$(function () {
 
-    'use strict';
+ 
+ /************************* JS Resize ***************************/
+    window.onload = indexSize;
+    window.onresize = indexSize;
 
-    /************************* JS Sidebar ***************************/
-    $('.js-menu-toggle').click(function (e) {
-
-        var $this = $(this);
-
-        if ($('body').hasClass('show-sidebar')) {
-            $('body').removeClass('show-sidebar');
-            $this.removeClass('active');
-        } else {
-            $('body').addClass('show-sidebar');
-            $this.addClass('active');
-        }
-
-        e.preventDefault();
-
-    });
-
-    // click outside offcanvas
-    $(document).mouseup(function (e) {
-        var container = $(".sidebar");
-        if (!container.is(e.target) && container.has(e.target).length === 0) {
-            if ($('body').hasClass('show-sidebar')) {
-                $('body').removeClass('show-sidebar');
-                $('body').find('.js-menu-toggle').removeClass('active');
-            }
-        }
-    });
-    
-    /************************* JS navbar ***************************/
-    window.onload = navBar;
-    window.onresize = navBar;
-
-    function  navBar() { 
-        let withWindow = parseInt(window.innerWidth);
-        let navbar = document.getElementById('navbarSupportedContent');
-        let toggleSidebar = document.getElementById('toggle-btn');
+    function  indexSize() { 
         let imgRoom2 = document.getElementById('imgRoom2');
         let imgRoom3 = document.getElementById('imgRoom3');
         let imgTest1 = document.getElementById('imgTest1');
@@ -46,18 +13,12 @@ $(function () {
         
 
         if (withWindow > 992) {
-            navbar.classList.add('disparition');
-            navbar.classList.remove( 'navbar-collapse');
-            toggleSidebar.classList.remove('disparition');
             imgRoom2.classList.remove('disparition');
             imgRoom3.classList.remove('disparition');
             imgTest1.style.position = "relative";
             whoWeAreContainer.style.border = "var(--color1) 2vh solid"
         }
         if (withWindow <= 992) {
-            navbar.classList.remove('disparition');
-            navbar.classList.add('navbar-expand-lg' , 'navbar-collapse');
-            toggleSidebar.classList.add('disparition');
             imgRoom2.classList.add('disparition');
             imgRoom3.classList.add('disparition');
             imgTest1.style.position = "static";
@@ -125,4 +86,3 @@ $(function () {
     };
 
 
-});
