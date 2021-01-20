290f91cd7e35f2bfbc935db6da9a822bd3a1663c
Deleau.E <edeleau@gmail.com>
2021-01-20 13:59:28 +0100

    Fix : bug avec le datepicker

--- assets/js/datepicker.js
+++ assets/js/datepicker.js
@@ -1,7 +1,14 @@
+import Datepicker from '../../node_modules/vanillajs-datepicker/js/Datepicker.js';
 import DateRangePicker from '../../node_modules/vanillajs-datepicker/js/DateRangePicker.js';
+import fr from '../../node_modules/vanillajs-datepicker/js/i18n/locales/fr.js';
+console.log(fr);
+Object.assign(Datepicker.locales, fr);
 
 
-const elem = document.getElementById('datePicker');
+const elem = document.getElementById('sandbox');
 const rangepicker = new DateRangePicker(elem, {
+    format :'dd/mm/yyyy',
+    language: 'fr',
     buttonClass: 'btn',
 }); 
+
--- reserve.php
+++ reserve.php
@@ -5,9 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HOTELCOP | Réservation</title>
+    <link rel="stylesheet" href="./node_modules/vanillajs-datepicker/dist/css/datepicker-bs4.min.css">
     <?php
     include("./link.php");
     ?>
+
+    <link rel="stylesheet" href="./assets/css/reserve.css">
+
 
 </head>
 
@@ -15,14 +19,26 @@
     <?php
     include("./nav.php");
     ?>
-    <div id="datePicker">
-        <input type="text" name="start">
-        <span>to</span>
-        <input type="text" name="end">
+
+    <div id="sandbox">
+        <div class="d-flex flex-row date input-daterange">
+            <div class="form-group">
+                <input type="text" name="range-start" class="form-control datepicker-input">
+            </div>
+            <div class="flex-grow-0">
+                <a class="btn" disabled="">to</a>
+            </div>
+            <div class="form-group">
+                <input type="text" name="range-end" class="form-control datepicker-input">
+            </div>
+        </div>
     </div>
+
     <?php
     include("./footer.php");
     ?>
+    <script type="module" src="./assets/js/datepicker.js"></script>
+
 </body>
 
 </html>
