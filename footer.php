8b59c4889c061092e751869e7dc4b03225006799
Pierre Duda <pierreduda@hotmail.fr>
2021-01-29 17:54:39 +0100

    CGV mentions légales

--- footer.php
+++ footer.php
@@ -6,8 +6,8 @@
                 <a href="access.php" class="col-12">Accès</a>
             </div>
             <div class="col-3 borderLeft row m-0 align-items-center">
-                <a href="" class="col-12">Mentions légales</a>
-                <a href="" class="col-12">CGV</a>
+                <a href="./mentions-legales.php" class="col-12">Mentions légales</a>
+                <a href="./cgv.php" class="col-12">CGV</a>
             </div>
             <div class="col-3 borderLeft row m-0 align-items-center">
                 <a href="recrutement.php" class="col-12 ">Recrutement</a>
